bff110b976d87b2cb190ffe743455d98d68f6182
abhi <abhilearnings2005@gmail.com>
2021-02-15 18:58:41 -0500

    test

---
+++ TestMavenJenkins/src/test/java/test1/com/TestJenkins.java
@@ -0,0 +1,12 @@
+package test1.com;
+
+public class TestJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Try SCM polling");
+
+	}
+
+}
